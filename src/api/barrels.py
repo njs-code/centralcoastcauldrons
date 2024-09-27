e730c9cddad7b325a45f533309dd8923f1d90c3e
Noah Snodgrass <419noah@protonmail.com>
2024-09-27 16:50:02 -0700

    Update barrels.py

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -30,7 +30,6 @@ def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
 @router.post("/plan")
 def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     """ """
-    print(wholesale_catalog)
 
     return [
         {
