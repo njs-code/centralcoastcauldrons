e228bb3cdc047f129006d6246b2b610877e4135a
Noah Snodgrass <419noah@protonmail.com>
2024-09-29 15:37:46 -0700

    Implemented audit to return global_inventory variables

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from src.api import auth
 import math
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
     prefix="/inventory",
@@ -12,8 +14,12 @@ router = APIRouter(
 @router.get("/audit")
 def get_inventory():
     """ """
-    
-    return {"number_of_potions": 0, "ml_in_barrels": 0, "gold": 0}
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory")).fetchall()
+        num_potions = result[0].num_green_potions
+        num_ml = result[0].num_green_ml
+        gold = result[0].gold
+    return {"number_of_potions": num_potions, "ml_in_barrels": num_ml, "gold": gold}
 
 # Gets called once a day
 @router.post("/plan")
