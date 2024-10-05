9946021a794f6ac1975f5c6fdf0863b471564c8e
Noah Snodgrass <419noah@protonmail.com>
2024-10-04 19:49:17 -0700

    Implemented plan and deliver functions with new databases

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -13,37 +13,35 @@ router = APIRouter(
 
 class Barrel(BaseModel):
     sku: str
-
     ml_per_barrel: int
     potion_type: list[int]
     price: int
-
     quantity: int
 
 @router.post("/deliver/{order_id}")
 def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
     """ """
-    #Version 1:  
-    # Update num_green_ml and gold based on barrels delivered. 
+    #Version 2: 
+    #Uses sku, volume, and quantity to update global inventory for each barrel
     total_price = 0
     with db.engine.begin() as connection:
         for barrel in barrels_delivered:
-            if barrel.sku == 'SMALL_GREEN_BARREL':
-                #calculate total ml of order 
-                green_ml = barrel.ml_per_barrel * barrel.quantity
-                inv_ml = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory")).scalar()
-                #update num_green_ml to inventory ml + delivered ml 
-                sql_to_execute = f"UPDATE global_inventory SET num_green_ml = '{green_ml + inv_ml}'"
-                connection.execute(sqlalchemy.text(sql_to_execute))
+            #determine color from SKU
+            color = barrel.sku.split("_")[1].lower()
+            #determine volume 
+            volume = barrel.ml_per_barrel * barrel.quantity
+            #select volume of liquid already in inventory 
+            inv_volume = connection.execute(sqlalchemy.text(f"SELECT {color} FROM global_inventory")).scalar()
+            #update inventory volume to add new barrel's volume
+            sql_to_execute = f"UPDATE global_inventory SET {color} = '{volume + inv_volume}'"
+            connection.execute(sqlalchemy.text(sql_to_execute))
             #keep track of price
             total_price += barrel.price
-            print(total_price)
         #update global_inventory gold based on price 
         inv_gold = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory")).scalar()
         sql_to_execute = f"UPDATE global_inventory SET gold = '{inv_gold - total_price}'"
         connection.execute(sqlalchemy.text(sql_to_execute))
     print(f"barrels delievered: {barrels_delivered} order_id: {order_id}")
-
     return "OK"
 
 # Gets called once a day
@@ -51,20 +49,16 @@ def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
 def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     """ """
     print(wholesale_catalog)
-    #Version 1: 
-    # Take num_green_potions and find out if more than 10 
-    # Request a small green barrel if <10 green potions
+    # Version 2:
+    # finds the potion in inventory with the smallest quantity 
+    # requests a barrel of this liquid 
     with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory")).fetchall()
-        num_green_potions = result[0].num_green_potions
-        num_green_ml = result[0].num_green_ml
-        gold = result[0].gold
-        if num_green_potions > 10 or gold < 50:
-            return []
-        else:
-            return [
+        least_quantity_potion = connection.execute(sqlalchemy.text("SELECT * FROM potions WHERE quantity=(SELECT MIN(quantity) from potions)")).fetchall()[0]
+        color = least_quantity_potion.sku.split("_")[1].upper()
+        barrel_sku = "SMALL_" + color + "_BARREL"
+        return [
                 {
-                    "sku": "SMALL_GREEN_BARREL",
+                    "sku": {barrel_sku},
                     "quantity": 1,
                 }
             ]
