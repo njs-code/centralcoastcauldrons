e16714fc13fbc0e01772bca09e5d31e8eb815a5c
Noah Snodgrass <419noah@protonmail.com>
2024-10-18 18:08:16 -0700

    Implemented database functions to get gold, potions, inventory volume

--- src/database.py
+++ src/database.py
@@ -12,3 +12,27 @@ engine = sqlalchemy.create_engine(database_connection_url(), pool_pre_ping=True)
 def get_table(table_name):
     metadata_obj = sqlalchemy.MetaData()
     return sqlalchemy.Table(table_name, metadata_obj, autoload_with=engine)
+
+def get_num_potions():
+    with engine.begin() as connection:
+        num_potions = connection.execute(
+            sqlalchemy.text(
+                "SELECT SUM(quantity) FROM potions AS sum"
+                )).scalar_one()
+    return num_potions
+
+def get_inv_volume():
+    with engine.begin() as connection:
+        return connection.execute(
+            sqlalchemy.text(
+                """SELECT SUM(red + green + blue + dark) 
+                FROM global_inventory"""
+                )).scalar_one()
+    
+def get_gold():
+    with engine.begin() as connection:
+        return connection.execute(
+            sqlalchemy.text(
+                """SELECT gold
+                FROM global_inventory"""
+                )).scalar_one()
