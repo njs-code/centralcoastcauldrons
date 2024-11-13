c244736300fd7c1ac8e095f533112afb5ccfedd4
Noah Snodgrass <419noah@protonmail.com>
2024-11-12 16:37:12 -0800

    audit error

--- src/api/admin.py
+++ src/api/admin.py
@@ -22,11 +22,24 @@ def reset():
         connection.execute(sqlalchemy.text("UPDATE global_inventory SET red=0, green=0, dark=0, blue=0, gold=100, potion_capacity=50, ml_capacity=10000"))
         #update potion quantities to 0
         connection.execute(sqlalchemy.text("UPDATE potions SET quantity = 0, num_sold=0"))
-        #clear all cart rows, if any
-        connection.execute(sqlalchemy.text("TRUNCATE TABLE cart_items"))
-        connection.execute(sqlalchemy.text("TRUNCATE TABLE orders"))
-        connection.execute(sqlalchemy.text("INSERT INTO orders (variety, order_id, gold_change, quantity) VALUES ('Purge',000,100,0)"))
         
+        #clear ledgers, carts, and orders
+        connection.execute(sqlalchemy.text("TRUNCATE TABLE ledger_potions, ledger_barrels, ledger_gold, orders"))
+        connection.execute(sqlalchemy.text("TRUNCATE TABLE cart_items"))
+
+
+        #refresh ledgers and orders
+        id = connection.execute(sqlalchemy.text
+                                ("""INSERT INTO orders 
+                                 (variety, order_id, gold_change, quantity) 
+                                 VALUES ('Purge',000,100,0) 
+                                 RETURNING id""")).scalar_one()
+        connection.execute(sqlalchemy.text
+                           ("""INSERT INTO ledger_gold 
+                            (quantity, order_id)
+                            VALUES(:quantity, :order_id)"""),
+                            [{"quantity":100, "order_id":id}])
+
         #reset cart counter to 1
         carts.id_counter = 0
 
